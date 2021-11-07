64582c761fc9c45b18750948c2e7c7df51823210
mazohere <83385567+mazohere@users.noreply.github.com>
2021-11-07 20:09:04 +1100

    finished
    completed

--- temperature_converter_script.py
+++ temperature_converter_script.py
@@ -4,6 +4,7 @@ import sys
 white = "\033[39m"
 red = "\033[31m"
 blue = "\033[34m"
+colour_val = "\033[39m"
 
 
 # function for after the conversion is finished so the program doesn't just end
@@ -18,23 +19,25 @@ def reset_function():
         exit()
 
     else:
-        print('invalid response')
+        print('invalid response, please try again')
         reset_function()
 
 # x <= 10: light blue, 10 < x <= 20: blue, 20 > x >= 30: red, x > 30: black
-def dependant_colour_variable_set(colour):
-    if colour < 10:
-        dependant_colour_variable = "\033[94m" # light_blue
+def dependant_colour_variable_set(colour, initial_val, end_val, convert):
 
-    elif 10 < colour <= 20:
-        dependant_colour_variable = "\033[34m" # blue
+    if colour <= 10:
+        colour_var = "\033[94m" # light_blue
+    elif 10 < colour <= 25:
+        colour_var = "\033[34m" # blue
+    elif 25 < colour <= 35:
+        colour_var = "\033[31m" # red
+    elif colour > 35:
+        colour_var = "\033[30m" # black
 
-    elif 20 > colour >= 30:
-        dependant_colour_variable = "\033[31m" # red
-    
-    elif colour > 30:
-        dependant_colour_variable = "\033[30m" # black
-
+    if convert == 1:
+        print(colour_var, initial_val, white, 'converted to F° is:', colour_var, end_val, white)
+    elif convert == 2:
+        print(colour_var, initial_val, white, 'converted to C° is:', colour_var, end_val, white)
 
 def temperature_converter():
     # inital print screen (should add colours later)
@@ -42,9 +45,16 @@ def temperature_converter():
     print('press ' + blue + '2 ' + white + 'to convert from ' + red + 'F° ' + white + 'to ' + red + 'C° ' + white)
 
     # error plan
-    def error():
-        print('incorrect')
-        temperature_converter()
+    def error(where):
+
+        print('invalid response, please try again')
+
+        if where == 1:
+            temperature_converter()
+        elif where == 2:
+            correct()
+        
+
 
     # user input recording and conversion
     choice = input(' ')
@@ -56,44 +66,38 @@ def temperature_converter():
             initial_value = input('enter initial temperature (C°): ')
             if initial_value.isnumeric() == True:
                 initial_value = int(initial_value)
-                print(initial_value)
-                print(type(initial_value))
-
-                dependant_colour_variable = "\033[39m"
-
-                dependant_colour_variable_set(initial_value)
-
                 end_value = (initial_value * 9/5) + 32
-                print(initial_value, 'converted to F° is:', end_value)
+
+                colour_value = initial_value
+
+                dependant_colour_variable_set(colour_value, initial_value, end_value, choice)
                 
                 reset_function()
 
             else:
-                error()
+                error(2)
 
         # F° route
         elif choice == 2:
             initial_value = input('enter initial temperature (F°): ')
             if initial_value.isnumeric() == True:
                 initial_value = int(initial_value)
-                print(initial_value)
-                print(type(initial_value))
-
                 end_value = (initial_value - 32) * 5/9
-                print(initial_value, 'converted to C° is:', end_value)
+
+                colour_value = initial_value
+
+                dependant_colour_variable_set(colour_value, initial_value, end_value, choice)
 
                 reset_function()
 
             else:
-                error()
+                error(2)
 
     # testing if the user input is correct
     if choice == '1' or choice == '2':
         choice = int(choice)
-        print(choice)
-        print(type(choice))
         correct()
     else:
-        error()
+        error(1)
 
 temperature_converter()
