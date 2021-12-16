a85bcae5b42ff44df93fada362c95aba43ecfed5
mazo_ <83385567+mazohere@users.noreply.github.com>
2021-12-16 18:39:06 +1100

    code comments added

--- temperature_converter_script.py
+++ temperature_converter_script.py
@@ -25,6 +25,7 @@ def reset_function():
 # x <= 10: light blue, 10 < x <= 20: blue, 20 > x >= 30: red, x > 30: black
 def dependant_colour_variable_set(colour, initial_val, end_val, convert):
 
+    #assigning the colour value for the final print screen
     if colour <= 10:
         colour_var = "\033[94m" # light_blue
     elif 10 < colour <= 25:
@@ -34,17 +35,18 @@ def dependant_colour_variable_set(colour, initial_val, end_val, convert):
     elif colour > 35:
         colour_var = "\033[30m" # black
 
+    # final print screen with colours based on the temperature and the converted temperature
     if convert == 1:
         print(colour_var, initial_val, white, 'converted to F° is:', colour_var, end_val, white)
     elif convert == 2:
         print(colour_var, initial_val, white, 'converted to C° is:', colour_var, end_val, white)
 
 def temperature_converter():
-    # inital print screen (should add colours later)
+    # inital print screen with colours
     print('press ' + blue + '1 ' + white + 'to convert from ' + red + 'C° ' + white + 'to ' + red + 'F° ' + white)
     print('press ' + blue + '2 ' + white + 'to convert from ' + red + 'F° ' + white + 'to ' + red + 'C° ' + white)
 
-    # error plan
+    # error function executing based on where it is called
     def error(where):
 
         print('invalid response, please try again')
@@ -61,6 +63,10 @@ def temperature_converter():
 
     # when the user input is correct
     def correct():
+        # both the C° and F° routes ask the user for their temperatures, convert the temperatures using their respective 
+        # formulas and set a colour value based on the temperature for a visual/graphical representation of the temperature 
+        # i.e light blue to red to black based on how hot it is
+
         # C° route
         if choice == 1:
             initial_value = input('enter initial temperature (C°): ')
@@ -93,7 +99,7 @@ def temperature_converter():
             else:
                 error(2)
 
-    # testing if the user input is correct
+    # testing if the user input is correct, converting it to an int if it is and calling the error function if it's not
     if choice == '1' or choice == '2':
         choice = int(choice)
         correct()
